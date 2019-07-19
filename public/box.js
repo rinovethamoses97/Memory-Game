356aaaac5f577e01fb829066929b6bfe37211f69
rino <rinovethamoses97@gmail.com>
2019-07-19 13:43:41 +0530

    Initial Commit

---
+++ public/box.js
@@ -0,0 +1,51 @@
+class Box{
+    constructor(x_,y_,size_,img_,imgId_){
+        this.x=x_;
+        this.y=y_;
+        this.size=size_;
+        this.angle=0;
+        this.rotate=false;
+        this.img=img_;
+        this.imgId=imgId_;
+        this.revealed=false;
+        this.completed=false;
+    }
+    update(boxes){
+        if(this.rotate){
+            this.angle+=5;
+            if(this.angle==180){
+                this.revealed=true;
+                this.rotate=false;
+                this.angle=0;
+                for(let i in boxes){
+                    if(boxes[i]!=this){
+                        if(!boxes[i].completed && this.imgId==boxes[i].imgId && boxes[i].revealed){
+                            this.completed=true;
+                            boxes[i].completed=true;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    show(){
+            push();
+            translate(this.x+this.size/2,0);
+            // displaying the axis about which the rectangle is rotated
+            // stroke(255);
+            // line(0,0,0,height);
+            rotateY(this.angle);
+            stroke(255);
+            fill(255);
+            if(this.revealed){
+                image(this.img,-this.size/2,this.y,this.size,this.size);
+            }
+            else
+                rect(-this.size/2,this.y,this.size,this.size);
+            pop()
+    }
+    isMouseIn(mouseX,mouseY){
+        return (mouseX>=this.x && mouseX<=this.x+this.size && mouseY>=this.y && mouseY<=this.y+this.size);
+    }
+}
---
+++ public/sketch.js
@@ -0,0 +1,59 @@
+let size=50;
+let rows=4;
+let cols=4;
+let boxes=[];
+let images=[];
+function preload(){
+    for(let i=0;i<8;i++)
+        images.push(loadImage("./musicInstruments/image"+(i+1)+".jpg"));
+}
+function setup(){
+    createCanvas(300,300,WEBGL);
+    background(0);
+    angleMode(DEGREES);
+    let options=[0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+    let gapY=20;
+    for(let i=0;i<rows;i++){
+        let gapX=20;
+        for(let j=0;j<cols;j++){
+            let index=floor(random(options.length));
+            let img=images[options[index]];
+            let imgId=options[index];
+            options.splice(index,1);
+            boxes.push(new Box((j*size)+gapX,(i*size)+gapY,size,img,imgId));
+            gapX+=20;
+        }
+        gapY+=20;
+    }
+}
+function draw(){
+    translate(-width/2,-height/2);
+    background(0);
+    let count=0;
+    for(let i in boxes){
+        boxes[i].update(boxes);
+        boxes[i].show();
+    }
+    for(let i in boxes){
+        if(!boxes[i].completed && boxes[i].revealed){
+            count++;
+        } 
+    }
+    if(count==2){
+        setTimeout(function(){
+            for(let i in boxes){
+                if(!boxes[i].completed && boxes[i].revealed){
+                    boxes[i].revealed=false;
+                }   
+            }
+        },200)
+    }
+}
+function mousePressed(){
+    for(let i in boxes){
+        if(boxes[i].isMouseIn(mouseX,mouseY) && !boxes[i].revealed){
+            boxes[i].rotate=true;
+            return;
+        }
+    }
+}
